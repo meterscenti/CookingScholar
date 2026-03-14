e30409836f101e632c2b97e9035ba3768e90d979
meterscenti <mcbraydenilao@gmail.com>
2026-03-14 12:54:53 +0800

    Added files needed for program

---
+++ ADELFA_Food_Database_COOKINGSCHOLAR.py
@@ -0,0 +1,24 @@
+Recipes = {1 :
+           """
+===Fish Fillet (15 minutes)===
+1. Cut the fish into strips.
+2. Dip in egg mixture, you can add seasonings if you like.
+3. Drop in some flour.
+4. Deep fry in high heat until golden brown.
+""",
+           2 :
+           """
+===Fried Rice (10 minutes)===
+1. Add oil to avoid sticking
+2. Put the rice in the hot pan
+3. Add seasonings like onions and soy sauce
+4. Stir until fragrant and cooked
+""",
+           3 :
+           """
+===Egg Sandwich (10 minutes)===
+1. Boil the eggs
+2. Mash the eggs until your desired consistency
+3. Mix in your dressing and seasonings
+4. Spread in a loaf of bread
+"""}
---
+++ ADELFA_Tutorial_COOKINGSCHOLAR.py
@@ -0,0 +1,63 @@
+def Tutorial():
+    
+    #    TUTORIAL
+    input(""" 
+                        -TEXT BOX-
+          
+    In the main page, the Text Boxes allows you to type in:
+        --> the ingredients you have
+        --> time available in minutes
+    Press ENTER to continue.""")
+
+    input(""" 
+    Example input:
+        Bread, Eggs, Milk
+        15
+    Press ENTER to continue.""")
+
+    #    allergen button
+    input(""" 
+                        -ALLERGEN BUTTON-
+          
+    If you have allergies, you can use the Allergen Button.
+    This can filter out recipes with unwanted ingredients like:
+        --> Milk
+        --> Nuts
+        --> Shrimp
+    Press ENTER to continue.""")
+
+    #    search engine
+    input(""" 
+                        -SEARCH ENGINE-
+          
+    Looking for something?
+    You can look for recipes with the Search Engine like:
+        --> Pork Empanada
+        --> Adobo
+        --> Pancakes
+    Press ENTER to continue.""")
+
+    #    food pages
+    input(""" 
+                        -FOOD PAGES-
+
+    From our database, the Food Pages shows you the information of the recipe you searched for.
+    Pages include:
+        --> Ingredients
+        --> Time to allot
+        --> Instructions
+    Press ENTER to continue.""")
+
+    #    download button + saved recipes
+    input(""" 
+                        -DOWNLOAD BUTTON AND SAVED RECIPES-
+
+    Found a recipe you absolutely love?
+    Press the Download Button to save it and access it even offline in the Saved Recipes!
+    Press ENTER to continue.""")
+
+    #    end
+    input(""" 
+    And that's the tutorial!
+          
+    It's time for you to start cooking. Have fun!""")
